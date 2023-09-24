1bf34ad8ee03692454907459c9ee7e9da1db61bf
Shubham <91934830+Shubham180401@users.noreply.github.com>
2023-09-24 11:35:52 +0530

    v44

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  // images: {
-  //   domains: [
-  //     'res.cloudinary.com',
-  //     'avatars.githubusercontent.com',
-  //     // 'imgur.com',
-  //     'drive.google.com',
-  //     'images.app.goo.gl',
-  //     'raw.githubusercontent.com',
-  //     'www.linkedin.com',
-  //     'media.licdn.com',
-  //   ],
-  // },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      // 'imgur.com',
+      'drive.google.com',
+      'images.app.goo.gl',
+      'raw.githubusercontent.com',
+      'www.linkedin.com',
+      'media.licdn.com',
+    ],
+  },
   output: "export",
   reactStrictMode: false,
 };
