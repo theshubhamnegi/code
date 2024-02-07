57f8d2c76ba05b88fdb0e5b2c7dc9546298e1003
Shubham <91934830+Shubham180401@users.noreply.github.com>
2024-02-07 21:38:54 +0530

    v71

--- components/Head.jsx
+++ components/Head.jsx
@@ -1,10 +1,8 @@
 import Head from 'next/head';
-import Icons from './icons/CodeIcon';
 
 const CustomHead = ({ title }) => {
   return (
     <Head>
-      <img src={Icons} alt={Icons} srcset="Icons" />
       <title>{title}</title>
     </Head>
   );
